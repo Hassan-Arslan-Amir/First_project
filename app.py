78aedfdc403f0daa8d6f70f2ec9d97c145f7e2e0
Hassan Arslan <hassanarslanamir@gmail.com>
2025-06-25 16:04:17 +0500

    Adding the condition in app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 count = 5
 if count <=5:
     for i in count:
         print(f"{i}: Hello World")
-=======
-# This is my first project
-count=5
-for i in count:
-    print (f"{i}:Hello World")
->>>>>>> b0519ac6e4ddf009ea4ac9b51961b7a808f6700a
